efcaeae551e24a706fb4df4f28a8d1051e0784eb
Naveen Rathi <102209027+Naveenrathi9@users.noreply.github.com>
2025-05-08 12:38:38 +0530

    Update hr_dashboard.js

--- it-request-system/frontend/hr_dashboard.js
+++ it-request-system/frontend/hr_dashboard.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://jpl-asset-form.onrender.com";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://jindal-power.onrender.com";
 
 
 // Global variables
